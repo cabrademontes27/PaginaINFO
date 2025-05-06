6cb2d14d060ed7dcbc2f50f6ae2efa053910cf95
cabrademontes <cabrademontes@gmail.com>
2025-05-06 13:42:46 -0600

    fix the issues for the base /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //solo para decir que el error siempre fue ./ esto debido que cargaba los assets desde la ruta actual no la raiz
+  // y por eso no mostraba nada ademas de que tambien agregamos vercerl.json
   base: '/',
 })
